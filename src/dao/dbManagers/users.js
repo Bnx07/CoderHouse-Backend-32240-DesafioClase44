6ccfd3a4accd3e7cd8a5579450f402c22272700e
Bnx07 <benjabastan@gmail.com>
2023-05-25 18:05:14 -0300

    Desafio sin finalizar y casi sin avances

---
+++ src/dao/dbManagers/users.js
@@ -0,0 +1,22 @@
+import userModel from "../models/users.js";
+
+export default class userManager {    
+    post = async(user) => {
+        let result = userModel.create(user);
+        return result;
+    }
+
+    getOne = async(search) => {
+        let result = userModel.findOne(search);
+        return result;
+    }
+
+    editOne = async(email, user) => {
+        let result = userModel.updateOne({email}, user);
+        return result;
+    }
+
+    editLastConnection = async(email, lastConnection) => {
+        let result = userModel.updateOne({email}, {$set: {last_connection: lastConnection}})
+    }
+}
